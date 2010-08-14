c5c0cd8646366e62a90c7b7c89f93451596d82a1
venj chu <ersaclarke@gmail.com>
2010-08-14 13:43:24 +0800

    Delete index for database compatibility.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100814042910) do
+ActiveRecord::Schema.define(:version => 20100813024046) do
 
   create_table "assets", :force => true do |t|
     t.string   "data_file_name"
@@ -45,9 +45,6 @@ ActiveRecord::Schema.define(:version => 20100814042910) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "posts", ["content"], :name => "index_posts_on_content"
-  add_index "posts", ["title"], :name => "index_posts_on_title"
 
   create_table "taggings", :force => true do |t|
     t.integer  "post_id"
